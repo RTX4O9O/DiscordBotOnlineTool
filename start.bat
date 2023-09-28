f20db7c2f7cd73257c52cd13c532315c173dedac
JayKuoNoob <jaykuo1203@gmail.com>
2023-09-28 23:47:54 +0800

    beta v2.4.2 first commit

---
+++ start.bat
@@ -0,0 +1,12 @@
+@echo off
+title DiscordBotOnlineTool-ver.b2.4
+echo    ___  _______  ______ ___  ___  ___ 
+echo   / _ ^\/_  __/ ^|/_/ / // _ ^\/ _ ^\/ _ ^\
+echo  / , _/ / / _^>  ^</_  _/ // /^\_, / // /
+echo /_/^|_^| /_/ /_/^|_^| /_/ ^\___//___/^\___/
+@echo on
+java -jar discordBotOnlineTool-beta-v2.4.jar
+pause
+@echo off
+cls
+pause
