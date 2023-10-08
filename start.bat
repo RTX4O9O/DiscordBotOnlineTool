b92648c769334a759996b39a2258bf52d71a41c5
JayKuoNoob <jaykuo1203@gmail.com>
2023-10-08 16:22:58 +0800

    v2.7 releasing

--- start.bat
+++ start.bat
@@ -1,11 +1,11 @@
 @echo off
-title DiscordBotOnlineTool-ver.b2.6
+title DiscordBotOnlineTool-ver.b2.7
 echo    ___  _______  ______ ___  ___  ___ 
 echo   / _ ^\/_  __/ ^|/_/ / // _ ^\/ _ ^\/ _ ^\
 echo  / , _/ / / _^>  ^</_  _/ // /^\_, / // /
 echo /_/^|_^| /_/ /_/^|_^| /_/ ^\___//___/^\___/
 @echo on
-java -jar discordBotOnlineTool-beta-v2.6.jar
+java -jar discordBotOnlineTool-beta-v2.7.jar
 @echo off
 cls
 echo discordBotOnlineTool has been stopped.
